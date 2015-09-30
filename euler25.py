b04e636b4eb1d189e2005c01e964067e20fac54a
DermotOHalloran <dermtt@gmail.com>
2015-09-30 18:12:00 +0000

    p2

---
+++ euler25.py
@@ -0,0 +1,11 @@
+a = 0
+b = 1
+c = 1 #a,b = fib variables, c = index counter
+
+while len(str(a)) < 1000: # using len to check if the amount of digits in a is 
+  b = b + a
+  a = b #fibonacci sequence
+  c += 1
+
+print (a)
+print (c) #prints a the current number, and c the index.
