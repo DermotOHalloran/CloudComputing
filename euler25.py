e84f44bc9e4f571968034a5cae92828404378376
DermotOHalloran <dermtt@gmail.com>
2015-09-30 18:16:42 +0000

    p4

--- euler25.py
+++ euler25.py
@@ -1,8 +1,9 @@
+## Python program to solve Euler Problem 25
 a = 0
 b = 1
 c = 1 #a,b = fib variables, c = index counter
 
-while len(str(a)) < 1000: # using len to check if the amount of digits in a is 
+while len(str(a)) < 1000: # using len to check if the amount of digits in a is
   b = b + a
   a = b #fibonacci sequence
   c += 1
