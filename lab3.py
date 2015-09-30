b870c4ad4ce8a41e31f8e192c5aa0dccfafa9889
DermotOHalloran <dermtt@gmail.com>
2015-09-30 18:20:08 +0000

    added comments

--- lab3.py
+++ lab3.py
@@ -1,6 +1,7 @@
-plndrm = input('Enter your potential pallindrome: ')
-if str(plndrm.lower()) == str(plndrm.lower())[::-1]:
+# Python Program to check if a string is a pallindrome
+plndrm = input('Enter your potential pallindrome: ')#User Input, defines variable
+if str(plndrm.lower()) == str(plndrm.lower())[::-1]: #Changes string to lowercase and compares it to its inverse
   print('True')
 else:
-  print('False')
+  print('False')# Prints outcome
 
