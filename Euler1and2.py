148dc88521b981af56899d42c201c82089e5d027
DermotOHalloran <dermtt@gmail.com>
2015-09-30 14:57:51 +0000

    comments fixed

--- Euler1and2.py
+++ Euler1and2.py
@@ -1,4 +1,4 @@
-// Euler Problem 1 Python
+##Euler Problem 1 Python
 
 for x in range(0,1000):
 	if x % 3 == 0 :
@@ -8,11 +8,11 @@ for x in range(0,1000):
 
 print sum
 
-/*I started of with the for loop as we are using numbers less than 1000, 
-next i used the if statement to test if it was divisible by 3 or 5 and if it was I added it to the sum.*/
+#I started of with the for loop as we are using numbers less than 1000, 
+#next i used the if statement to test if it was divisible by 3 or 5 and if it was I added it to the sum.*/
 
 
-//Euler Problem 2 Python
+#Euler Problem 2 Python
 
 a = 0
 b = 1
@@ -24,7 +24,7 @@ while a < 4000000
 		 sum = sum + a
 print sum
 
-/*First I defined the first two numbers , next I defined range in a while loop, finally I added the fibonacci sequence where the next number is the sum of the last two, if the number is even it gets added to the sum.*/
+#First I defined the first two numbers , next I defined range in a while loop, finally I added the fibonacci sequence where## the next number is the sum of the last two, if the number is even it gets added to the sum.*/
 	
 
 
